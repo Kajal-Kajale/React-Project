04064eac00592f791ccb8eb6cdb00d5fea08e257
unknown <kkajale10@gmail.com>
2021-07-29 19:53:55 +0530

    changes

--- Desktop/react-project/src/Test.js
+++ Desktop/react-project/src/Test.js
@@ -1,1 +1,10 @@
-//Testing git upload
+//Testing git upload $ git push
+// Enumerating objects: 10, done.
+// Counting objects: 100% (10/10), done.
+// Delta compression using up to 4 threads
+// Compressing objects: 100% (3/3), done.
+// Writing objects: 100% (6/6), 429 bytes | 214.00 KiB/s, done.
+// Total 6 (delta 2), reused 0 (delta 0), pack-reused 0
+// remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+// To https://github.com/Kajal-Kajale/React-Project.git
+//    b2da184..450f34f  main -> main
